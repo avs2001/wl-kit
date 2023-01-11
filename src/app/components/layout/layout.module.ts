45ea3b2360beae3f1c9d96e6a01a97acb6fbf0aa
iraklis <iraklis.bekiaris@kebormed.com>
2023-01-11 16:07:03 +0200

    wip

--- src/app/components/layout/layout.module.ts
+++ src/app/components/layout/layout.module.ts
@@ -4,7 +4,6 @@ import { DrawerComponent } from './drawer/drawer.component';
 import { HeaderComponent } from './header/header.component';
 import { SideComponent } from './side/side.component';
 import { ToolbarComponent } from './toolbar/toolbar.component';
-import { SideItemComponent } from './side/side-item/side-item.component';
 
 @NgModule({
     imports: [
@@ -20,9 +19,6 @@ import { SideItemComponent } from './side/side-item/side-item.component';
         HeaderComponent,
         SideComponent,
         ToolbarComponent
-    ],
-    declarations: [
-      SideItemComponent
     ]
 })
 export class LayoutModule {
