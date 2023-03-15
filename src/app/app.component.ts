27dfabc6f1d01bf7cbeb15099c2bbcb1bcde88ec
Ciprian Iuga <ciprian@kebormed.com>
2023-03-15 17:50:38 +0200

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {CommonModule} from "@angular/common";
+import {HttpClient} from "@angular/common/http";
+
+export function users$() {
+  return inject(HttpClient).get<any[]>('https://jsonplaceholder.typicode.com/users')
+}
 
 @Component({
   selector: 'kbm-root',
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'wl-kit';
+  users$ = users$();
 }
